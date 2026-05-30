7e0cc9bd7ae09d6c4a07b02e541f11e637e3694c
Samuel Hierholzer <samuel@ton-kunst.ch>
2026-05-30 06:18:57 +0200

    feat(history): Only show a single command

--- history.go
+++ history.go
@@ -18,11 +18,11 @@ var (
 )
 
 type history struct {
-	commands        []shell.Command
-	views           []viewport.Model
-	focusedViewport int
-	isFocussed      bool
-	Width           int
+	command    shell.Command
+	view       viewport.Model
+	isFocussed bool
+	Width      int
+	Height     int
 }
 
 var (
@@ -30,11 +30,8 @@ var (
 	darkGreenGut   = lipgloss.NewStyle().Foreground(lipgloss.Color("22")) // dark green
 )
 
-// TODO: history should only track one shell?
 func newHistory() *history {
-	return &history{
-		focusedViewport: 0,
-	}
+	return &history{}
 }
 
 func (h *history) Init() tea.Cmd {
@@ -44,100 +41,63 @@ func (h *history) Init() tea.Cmd {
 func (h *history) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyPressMsg:
-		switch msg.String() {
-		case "up", "k":
-			if h.focusedViewport > 0 {
-				h.views[h.focusedViewport].LeftGutterFunc = unselected
-				h.focusedViewport--
-				h.views[h.focusedViewport].LeftGutterFunc = selected
-			}
-			return h, nil
-		case "down", "j":
-			if h.focusedViewport < len(h.views)-1 {
-				h.views[h.focusedViewport].LeftGutterFunc = unselected
-				h.focusedViewport++
-				h.views[h.focusedViewport].LeftGutterFunc = selected
-			}
-			return h, nil
-		case "ctrl+c":
-			return h, func() tea.Msg { return RequestFocusMainMsg{} }
+		if h.isFocussed {
+			var cmd tea.Cmd
+			h.view, cmd = h.view.Update(msg)
+			return h, cmd
 		}
 
 	case shell.NewCommandMsg:
-		h.Add(msg.Cmd)
+		h.command = msg.Cmd
+		h.view = viewport.New(
+			viewport.WithWidth(h.Width),
+			viewport.WithHeight(max(0, h.Height-1)),
+		)
+		h.view.FillHeight = false
+		h.view.LeftGutterFunc = selected
+		h.updateContent()
 		return h, nil
 
 	case tea.WindowSizeMsg:
 		h.Width = msg.Width
-		for _, view := range h.views {
-			view.SetWidth(msg.Width)
-		}
+		h.Height = msg.Height
+		h.view.SetWidth(msg.Width)
+		h.view.SetHeight(max(0, h.Height-1))
 		return h, nil
 
 	case shell.StdoutMsg:
 		log.Print("Stdout output received")
-		for i, c := range h.commands {
-			if c == msg.Cmd {
-				h.updateViewportContent(i)
-				break
-			} else {
-				log.Printf("%q is not command %q", c, msg.Cmd)
-			}
+		if h.command == msg.Cmd {
+			h.updateContent()
 		}
 		return h, nil
 
 	case tea.BlurMsg:
-		if len(h.views) == 0 {
-			return h, nil
-		}
-		h.views[h.focusedViewport].LeftGutterFunc = unselected
+		h.view.LeftGutterFunc = unselected
 		h.isFocussed = false
 
 	case tea.FocusMsg:
-		if len(h.views) == 0 {
-			return h, nil
-		}
-		h.views[h.focusedViewport].LeftGutterFunc = selected
+		h.view.LeftGutterFunc = selected
 		h.isFocussed = true
 		return h, nil
-		// TODO: A view for stderr!
-		//case shell.StderrMsg:
-		//	log.Print("Stderr output received")
-		//	for i, c := range h.commands {
-		//		if c == msg.Cmd {
-		//			h.updateViewportContent(i)
-		//			break
-		//		}
-		//	}
 	}
 
 	return h, nil
 }
 
 func (h *history) View() tea.View {
-	var strs []string
-	for _, view := range h.views {
-		strs = append(strs, view.View())
+	if h.command == nil {
+		return tea.NewView("")
 	}
-	return tea.NewView(lipgloss.JoinVertical(lipgloss.Left, strs...))
+	return tea.NewView(h.command.CommandLine() + "\n" + h.view.View())
 }
 
-func (h *history) Add(cmd shell.Command) {
-	h.commands = append(h.commands, cmd)
-
-	commandView := viewport.New(
-		viewport.WithWidth(h.Width),
-	)
-	//commandView.YPosition = 0
-	commandView.FillHeight = false
-	if len(h.views) > 0 {
-		h.views[len(h.views)-1].LeftGutterFunc = unselected
+func (h *history) updateContent() {
+	if h.command == nil {
+		return
 	}
-	commandView.LeftGutterFunc = selected
-	h.views = append(h.views, commandView)
-	h.focusedViewport = len(h.views) - 1
-
-	h.updateViewportContent(len(h.views) - 1)
+	output := strings.Trim(h.command.Stdout(), "\r\n")
+	h.view.SetContent(output)
 }
 
 func unselected(ctx viewport.GutterContext) string {
@@ -146,14 +106,4 @@ func unselected(ctx viewport.GutterContext) string {
 
 func selected(ctx viewport.GutterContext) string {
 	return darkGreenGut.Render("│")
-}
-
-func (h *history) updateViewportContent(i int) {
-	if i < 0 || i >= len(h.commands) {
-		return
-	}
-	command := h.commands[i]
-	content := command.CommandLine() + "\n" + strings.Trim(command.Stdout(), "\r\n")
-	h.views[i].SetHeight(lipgloss.Height(content))
-	h.views[i].SetContent(content)
 }
